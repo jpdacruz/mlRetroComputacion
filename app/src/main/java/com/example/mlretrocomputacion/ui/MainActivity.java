08f543a19f5aca2706327e084a2126e9852c42e9
dacruzfindo <jrodrigues@findo.com.ar>
2021-01-14 17:03:56 -0300

    remove bottom nav nav add method for navigate in app (<- in app bar)

--- app/src/main/java/com/example/mlretrocomputacion/ui/MainActivity.java
+++ app/src/main/java/com/example/mlretrocomputacion/ui/MainActivity.java
@@ -8,6 +8,7 @@ import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,11 +18,21 @@ import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home)
+                .build();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController,(DrawerLayout)null);
     }
 }
